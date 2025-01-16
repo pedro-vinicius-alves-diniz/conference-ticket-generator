1df67028b9d73d9d7bb7409290350baca17375ff
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2025-01-16 23:33:59 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'n-0': 'hsl(0, 0%, 100%)',
+        'n-300': 'hsl(252, 6%, 83%)',
+        'n-500': 'hsl(245, 15%, 58%)',
+        'n-700': 'hsl(245, 19%, 35%)',
+        'n-900': 'hsl(248, 70%, 10%)',
+        'orange-5': 'hsl(7, 88%, 67%)',
+        'orange-7': 'Orange 700: hsl(7, 71%, 60%)'
+      },
+      fontFamily: {
+        'incosolata': ['Inconsolata', 'sans-serif'],
+      },
+
+      backgroundImage: {
+        'mobile': 'url(/src/assets/images/background-mobile.png)',
+        'tablet': 'url(/src/assets/images/background-tablet.png)',
+        'desktop': 'url(/src/assets/images/background-desktop.png)'
+      }
+    },
   },
   plugins: [],
 }
