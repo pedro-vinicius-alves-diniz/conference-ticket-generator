79a1a7eb22ed9280dfe814866c3fbf9eac934253
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2025-01-22 15:27:02 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ module.exports = {
         'n-700': 'hsl(245, 19%, 35%)',
         'n-900': 'hsl(248, 70%, 10%)',
         'orange-5': 'hsl(7, 88%, 67%)',
-        'orange-7': 'hsl(7, 71%, 60%)'
+        'orange-7': 'hsl(7, 71%, 60%)',
+        'gradient-from': 'hsl(7, 86%, 67%)',
+        'gradiente-to': 'hsl(0, 0%, 100%)'
       },
       fontFamily: {
         'incosolata': ['Inconsolata', 'sans-serif'],
@@ -19,7 +21,11 @@ module.exports = {
       backgroundImage: {
         'mobile': 'url(/src/assets/images/background-mobile.png)',
         'tablet': 'url(/src/assets/images/background-tablet.png)',
-        'desktop': 'url(/src/assets/images/background-desktop.png)'
+        'desktop': 'url(/src/assets/images/background-desktop.png)',
+        'ticket': 'url(/src/assets/images/pattern-ticket.svg)'
+      },
+      aspectRatio: {
+        'ticket': '15 / 7'
       }
     },
   },
